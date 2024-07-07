9986644c8fed8f0de932cb87c6696cd8ba055354
d_d_k <102277507+DDK0001@users.noreply.github.com>
2024-07-07 13:30:05 +0530

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,9 +4,10 @@ lua54 'yes'
 
 author 'dk.kratos'
 description 'QB Interfaces Compatibility for OverExtended\'s Resources' 
-version '1.3.1'
+version '1.0.0'
 
 client_script 'client/*.lua'
+server_script 'versioncheck.lua'
 shared_scripts {
     'config.lua',
     '@ox_lib/init.lua',
