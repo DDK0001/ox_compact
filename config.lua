baf57014cb1cd4a5a08e9c4ec501f8cdd9cfad98
D_D_K <102277507+DDK0001@users.noreply.github.com>
2025-03-02 13:07:26 +0530

    Update config.lua

--- config.lua
+++ config.lua
@@ -10,6 +10,10 @@ Config.Modules = {
         active = true, -- Do you need this module to be enabled?
         resource_name = 'qb-input' -- What is the name of the resource that provided this module? (In case you changed its name)
     },
+   ['qb-target'] = {
+        active = true, -- Do you need this module to be enabled?
+        resource_name = 'qb-target' -- What is the name of the resource that provided this module? (In case you changed its name)
+    },
 }
 
 Config.InventoryName = 'qb-inventory'
